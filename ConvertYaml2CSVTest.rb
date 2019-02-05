28c7a4dc4b1005b8de7de5934fc43639cd5b4a67
RayNieva <raynieva@gmail.com>
2019-02-04 20:45:40 -0500

    testing git back-up automation

---
+++ ConvertYaml2CSVTest.rb
@@ -0,0 +1,25 @@
+=begin
+This begins by taking a YAML formatted file converting to an array and then taking that
+array that will have hash elements and converting values to CSV format while also generating  column headers
+=end
+require 'yaml'
+#file=File.new("data.yaml")
+file=File.new("test.yaml")
+array=YAML.load(file)
+file.close
+p array
+
+# For column headers and multiple hashes
+require 'csv'
+hashes=array
+column_names=hashes.first.keys
+h="test hello there\n"
+p h
+File.write('the_file.csv',h)
+s=CSV.generate do |csv|
+  csv << column_names
+  hashes.each do |x|
+    csv << x.values
+  end
+end
+File.write('the_file.csv', s, mode: 'a')
